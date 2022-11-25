e2c7190b402963769bcac37b2752d4a02b14ee92
mahdi-frmz <faramarzpour98@gmail.com>
2022-11-25 10:03:11 +0330

    restructure user module

--- packages/user/src/lib/user.ts
+++ packages/user/src/lib/user.ts
@@ -1,18 +1,7 @@
-import * as storage from '@chut/storage'
+import * as service from './service'
+import * as controller from './controller'
 
-interface User {
-  username: string,
-  password: string
-}
-
-export async function userCreate(user: User) {
-  const user_string = JSON.stringify(user)
-  await storage.set(user.username, user_string)
-}
-
-export async function userGet(username: string): Promise<User | null> {
-  const user_string = await storage.get(username)
-  if (!user_string)
-    return null;
-  return JSON.parse(user_string);
+export {
+  service,
+  controller
 }
