c3c8dc60ec967ea01aa6f3dbc37e49f16f959e45
mahdi-frmz <faramarzpour98@gmail.com>
2022-11-25 13:20:34 +0330

    refactor

---
+++ packages/services/auth/README.md
@@ -0,0 +1,11 @@
+# services-auth
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build services-auth` to build the library.
+
+## Running unit tests
+
+Run `nx test services-auth` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/services/auth/src/lib/auth.ts
@@ -0,0 +1,24 @@
+import * as UserService from '@chut/user';
+import { User } from '@chut/types';
+import * as jwt from 'jsonwebtoken';
+
+const JWT_KEY = 'my secret jwt key';
+const JWT_ALG = 'RS256';
+
+export async function userAuth(
+  username: string,
+  password: string
+): Promise<User | null> {
+  const user = await UserService.userGet(username);
+  if (!user || user.password != password) return null;
+  return user;
+}
+
+export async function generateToken(user: User): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    jwt.sign(user, JWT_KEY, { algorithm: JWT_ALG }, (err, tkn) => {
+      if (err || !tkn) reject(err);
+      else resolve(tkn);
+    });
+  });
+}
