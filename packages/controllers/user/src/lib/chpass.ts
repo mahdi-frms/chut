702e47b3f93b0f79581b121039425d130b23c16e
mahdi-frmz <faramarzpour98@gmail.com>
2022-11-28 16:25:41 +0330

    chpass impl

--- packages/controllers/user/src/lib/chpass.ts
+++ packages/controllers/user/src/lib/chpass.ts
@@ -1,13 +1,21 @@
-import * as Auth from '@chut/auth'
 import { Status } from '@chut/types';
 import { Router } from 'express'
-import { jwtAuth } from '@chut/mid-auth'
+import { jwtAuth, reqUser } from '@chut/mid-auth'
+import { userUpdate } from '@chut/user';
 
 const route = Router();
 route.use(jwtAuth)
 
 route.use(async (req, res) => {
-    res.end('hi!');
+    const { oldPassword, newPassword } = req.body;
+    const user = reqUser(req);
+    if (user.password != oldPassword)
+        res.status(401).send({ status: Status.InvalidCredentials });
+    else {
+        user.password = newPassword;
+        await userUpdate(user);
+        res.status(200).send({ status: Status.Sucess });
+    }
 })
 
 export default route;
--- packages/midwares/auth/src/lib/midwares-auth.ts
+++ packages/midwares/auth/src/lib/midwares-auth.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express'
-import { Status } from '@chut/types'
+import { Request, Router } from 'express'
+import { Status, User } from '@chut/types'
 import * as Auth from '@chut/auth'
 
 export const jwtAuth = Router()
+
+export function reqUser(req: Request): User {
+  return ((req as unknown) as { user: User }).user
+}
 
 jwtAuth.use(async (req, res, next) => {
   const tkn = req.cookies['jwt-token'];
@@ -12,7 +16,9 @@ jwtAuth.use(async (req, res, next) => {
     const jwtAuthResult = await Auth.jwtAuth(tkn);
     if (!jwtAuthResult)
       res.status(401).send({ status: Status.InvalidToken });
-    else
+    else {
+      ((req as unknown) as { user: User }).user = jwtAuthResult;
       next()
+    }
   }
 })
